121e99a74237a96003a3be6fcdc8082297a9e091
Vadim Shidlov <mat23vadim@mail.ru>
2023-05-23 16:37:46 +0300

    feat: complete tasks from 02-numbers-tasks.js file

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -7,7 +7,6 @@
  *                                                                                           *
  ******************************************************************************************* */
 
-
 /**
  * Returns an area of a rectangle given by width and height.
  *
@@ -19,10 +18,9 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(/* width, height */) {
-  throw new Error('Not implemented');
+function getRectangleArea(width, height) {
+  return width * height;
 }
-
 
 /**
  * Returns a circumference of circle given by radius.
@@ -35,9 +33,11 @@ function getRectangleArea(/* width, height */) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCircleCircumference(/* radius */) {
-  throw new Error('Not implemented');
+function getCircleCircumference(radius) {
+  return 2 * radius * Math.PI;
 }
+
+// console.log(getCircleCircumference(5));
 
 /**
  * Returns an average of two given numbers.
@@ -51,9 +51,10 @@ function getCircleCircumference(/* radius */) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(/* value1, value2 */) {
-  throw new Error('Not implemented');
+function getAverage(value1, value2) {
+  return (value1 + value2) / 2;
 }
+// console.log(getAverage(-3, 3));
 
 /**
  * Returns a distance between two points by cartesian coordinates.
@@ -70,9 +71,12 @@ function getAverage(/* value1, value2 */) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+// d = √((хА –  хВ)2 + (уА – уВ)2)
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
 }
+
+// console.log(getDistanceBetweenPoints(0, 0, 1, 0));
 
 /**
  * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
@@ -86,10 +90,12 @@ function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a, b) {
+  const r = -b / a;
+  return r;
 }
 
+// console.log(getLinearEquationRoot(5, -10));
 
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi,
@@ -125,10 +131,12 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const str = String(value);
+  return Number(str[str.length - 1]);
 }
 
+// console.log(getLastDigit(37));
 
 /**
  * Returns a number by given string representation.
@@ -141,8 +149,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -158,10 +166,11 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
+// console.log(getParallelepipedDiagonal(1, 1, 1));
 
 /**
  * Returns the number rounded to specified power of 10.
@@ -180,9 +189,11 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / Math.pow(10, pow)) * Math.pow(10, pow);
 }
+
+// console.log(roundToPowerOfTen(1234, 1));
 
 /**
  * Returns true is the number is prime; otherwise false.
@@ -201,9 +212,23 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if (n <= 1) {
+    return false;
+  }
+
+  if (n === 2 || n === 3) return true;
+
+  const res = [];
+  for (let i = 1; i <= n; i++) {
+    if (n % i === 0) {
+      res.push(true);
+    }
+  }
+  return !(res.length > 2);
 }
+
+// console.log(isPrime(17));
 
 /**
  * Tries to convert value to number and returns it if conversion was successful;
